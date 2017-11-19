7a7b75d69b6782e8277281222f8a7b859063aa7c
hepengqi <pengqihe@babeltime.com>
2017-11-20 01:28:47 +0800

    添加事件系统

--- src/Main.lua
+++ src/Main.lua
@@ -18,6 +18,32 @@ local sudoku = require("Sudoku").new({
     0,0,0,0,2,0,1,4,7,
 })
 
-print(sudoku:output())
-sudoku:checkDirty()
-print(sudoku:output())
+--print(sudoku:output())
+--sudoku:checkDirty()
+--print(sudoku:output())
+
+local a = require("Event.EventDispatcher").new()
+local b = {}
+local c = {}
+setmetatable(c, {__mode = "k"})
+c[b] = 1
+
+b.ff = function(self, ed)
+    print("1", ed:getTarget() == a, ed:getData())
+    --print(ed:getData())
+end
+
+local f2 = function()
+    print(2)
+end
+
+a:addEventListener("ff", b.ff, nil, 0, true)
+--a:removeEventListener("ff", b.ff, nil, 0, true)
+--a:addEventListener("ff", f2, nil, 1, false)
+b = nil
+collectgarbage()
+for t, v in pairs(c) do
+    print(t)
+end
+a:dispatchEvent("ff", "gasdf")
+
